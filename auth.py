f044894f144585d7464320082ea4efa4977d6b6b
jc4p <admin@celsior.org>
2011-08-16 12:52:26 -0700

    Added auth file

---
+++ auth.py
@@ -0,0 +1,13 @@
+auth.mail_server = "mail.yourserver.com" # your IMAP server
+auth.mail_address = "you@you.com" # The e-mail receiving Google Voice forwarding messages
+auth.mail_password = "password" # Your e-mail password
+
+auth.mysql_server = "db.yourserver.com" # Your MySQL server
+auth.mysql_user = "gVoiceUser" # Your MySQL user
+auth.mysql_password = "password" # Your MySQL password
+auth.mysql_db = "gVoiceDb" # The database that contains this program's storage
+
+auth.twitter_consumer = "consumer_key" # Your Twitter API consumer key
+auth.twitter_secret = "secret" # Your Twitter API secret key
+auth.twitter_token = "user_token" # Your Twitter Single-user Token
+auth.twitter_token_secret = "token_secret" # Your Twitter Single-user Secret
